5ec0193d78897a06557bd306e04683d9637ebf94
daddyrusher <daddyrusher@gmail.com>
2021-05-05 00:23:58 +0300

    list owners on owner index page closes #26

--- pet-clinic-web/src/main/java/com/daddyrusher/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/daddyrusher/petclinic/controllers/OwnerController.java
@@ -1,15 +1,24 @@
 package com.daddyrusher.petclinic.controllers;
 
+import com.daddyrusher.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping
+@RequestMapping("/owners")
 public class OwnerController {
 
-    @GetMapping({"/owners", "/owners/index", "/owners/index.html"})
-    public String listOwners() {
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @GetMapping({"/", "/index", "/index.html"})
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
