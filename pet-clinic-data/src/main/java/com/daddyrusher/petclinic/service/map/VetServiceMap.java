3f01056569fcfe0591654c652ba99f3be930485c
daddyrusher <daddyrusher@gmail.com>
2021-05-04 23:55:47 +0300

    added bootstrap for data closes #24

--- pet-clinic-data/src/main/java/com/daddyrusher/petclinic/service/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/daddyrusher/petclinic/service/map/VetServiceMap.java
@@ -1,33 +1,33 @@
 package com.daddyrusher.petclinic.service.map;
 
 import com.daddyrusher.petclinic.model.Vet;
-import com.daddyrusher.petclinic.service.CrudService;
+import com.daddyrusher.petclinic.service.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
-        return this.findAll();
+        return super.findAll();
     }
 
     @Override
     public void deleteById(Long id) {
-        this.deleteById(id);
+        super.deleteById(id);
     }
 
     @Override
     public void delete(Vet object) {
-        this.delete(object);
+        super.delete(object);
     }
 
     @Override
     public Vet save(Vet object) {
-        return this.save(object);
+        return super.save(object.getId(), object);
     }
 
     @Override
     public Vet findById(Long id) {
-        return this.findById(id);
+        return super.findById(id);
     }
 }
