b8de75068ecbca37605e3232da9748834a7a397d
daddyrusher <daddyrusher@gmail.com>
2021-05-05 00:13:26 +0300

    added service configuration closes #36

--- pet-clinic-data/src/main/java/com/daddyrusher/petclinic/service/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/com/daddyrusher/petclinic/service/map/OwnerServiceMap.java
@@ -2,9 +2,11 @@ package com.daddyrusher.petclinic.service.map;
 
 import com.daddyrusher.petclinic.model.Owner;
 import com.daddyrusher.petclinic.service.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
     public Set<Owner> findAll() {
