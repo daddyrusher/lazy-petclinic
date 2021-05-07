7fb76febada061b63c60c9535b5327dc7e501e4d
daddyrusher <daddyrusher@gmail.com>
2021-05-07 23:30:29 +0300

    implemented im-memory service for visit closes #99

--- pet-clinic-data/src/main/java/com/daddyrusher/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/daddyrusher/petclinic/model/BaseEntity.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+import static java.util.Objects.isNull;
+
 @Getter
 @Setter
 @MappedSuperclass
@@ -17,4 +19,6 @@ public class BaseEntity implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    public boolean isNew() { return isNull(getId()); }
 }
---
+++ pet-clinic-data/src/main/java/com/daddyrusher/petclinic/service/inmemory/ImMemoryVisitService.java
@@ -0,0 +1,39 @@
+package com.daddyrusher.petclinic.service.inmemory;
+
+import com.daddyrusher.petclinic.exception.EntityException;
+import com.daddyrusher.petclinic.model.Visit;
+import com.daddyrusher.petclinic.service.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class ImMemoryVisitService extends AbstractInMemoryService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        if (object.isNotValid()) {
+            throw new EntityException("Invalid Visit!");
+        }
+        return super.save(object);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
