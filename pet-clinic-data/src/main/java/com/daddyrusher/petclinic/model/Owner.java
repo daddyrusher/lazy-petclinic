fa4049ed9d77b969a95f07a18e263a8c7d6e0e82
Zanin Maksim <daddyrusher@gmail.com>
2021-05-07 15:44:48 +0300

    Revert "Feature/owners to jpa"

--- pet-clinic-data/src/main/java/com/daddyrusher/petclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/com/daddyrusher/petclinic/model/Owner.java
@@ -3,23 +3,14 @@ package com.daddyrusher.petclinic.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
 import java.util.HashSet;
 import java.util.Set;
 
 @Getter
 @Setter
-@Entity
-@Table(name = "owners")
 public class Owner extends Person {
-
     private String address;
     private String city;
     private String phone;
-
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")
     private Set<Pet> pets = new HashSet<>();
 }
